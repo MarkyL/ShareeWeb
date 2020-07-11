d3956fdfd0aa28285ac68b6b686353e4495243ba
Mark Lurie <Mark.lu91@gmail.com>
2020-07-11 15:44:09 +0300

    - Some more github required commit.

---
+++ build/precache-manifest.43e89680dadbf3fd829e4bb0406505cb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "626e4ef2bee26c4e666a",
+    "url": "/static/css/main.074df260.chunk.css"
+  },
+  {
+    "revision": "3ba66cca2056dd6d79e7",
+    "url": "/static/js/runtime~main.3ba66cca.js"
+  },
+  {
+    "revision": "cd3f84e9fabfbe22f8c63edef3e769e7",
+    "url": "/static/media/bg_404.cd3f84e9.png"
+  },
+  {
+    "revision": "cd75cf2a1c28c0e70f38",
+    "url": "/static/js/2.cd75cf2a.chunk.js"
+  },
+  {
+    "revision": "fec6134b9773126fdc216a985067485f",
+    "url": "/static/media/circle.fec6134b.svg"
+  },
+  {
+    "revision": "626e4ef2bee26c4e666a",
+    "url": "/static/js/main.626e4ef2.chunk.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "59239125df727680fab97988c7d821b9",
+    "url": "/static/media/sharee_logo.59239125.png"
+  },
+  {
+    "revision": "cd75cf2a1c28c0e70f38",
+    "url": "/static/css/2.96a2934f.chunk.css"
+  },
+  {
+    "revision": "eb218440785f751f1d6929ff4a2de20a",
+    "url": "/index.html"
+  }
+];
